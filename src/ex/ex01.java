416cac69ac86df75fb17cadb312eeab21c7ccb2a
q9002285117 <105021038@live.asia.edu.tw>
2016-11-28 16:05:30 +0800

    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,18 +1,39 @@
 ﻿package ex;
+
+import java.util.Random;
+import java.util.Scanner;
+
 /*
  * Topic: 請設計一樂透亂數選號程式，由 1~42 中選出 6個不重覆的數字組合並輸出。 請使用已下方式,(setSeed就是設定亂數種子，可以使每次跑出的亂數序列，都會是一樣的)
  例如:輸入 23323456會得到 39  17  32  13  41  15 
 	Random randnum = new Random();
 	randnum.setSeed(23323456)
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021038 傅琬鈞
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		Random randnum = new Random();
+		int n = scn.nextInt();
+		randnum.setSeed(n);
+		int data[] = new int[6];
+		for (int i = 0; i <= 5; i++) {
+			data[i] = randnum.nextInt(42) + 1;
+			for (int j = 0; j < i; j++) {
+				if (data[i] == data[j]) {
+					data[i] = randnum.nextInt(42) + 1;
+				}
+			}
+		}
+		for (int i = 0; i <= 5; i++) {
+			System.out.print(data[i] + "\t");
+		}
+		System.out.println();
 	}
 
 }
+
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -4,14 +4,69 @@
  並於主程式讓使用者輸入位學生的資料並根據平均成績由高至低輸出如下:
  ID    NAME     Chinese     English       Math        Avg
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021038 傅琬鈞
  */
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+				Scanner scn=new Scanner(System.in);
+				ArrayList<Student>studList=new ArrayList<Student>();
+		studList.add(new Student("David","1899"));
+		studList.add(new Student("Jack","1085"));
+		studList.add(new Student("Kevin","1985"));
+		studList.get(1).showInfo();
+		studList.get(1).addCourse("程式1","1050807");
+		studList.get(1).showInfo();
 
-	}
+			}
 
-}
+		}
+		
+
+		class Student {
+
+			private String id, name;
+			private ArrayList<Course> couList = new ArrayList<Course>();
+
+			public Student(String name1, String id1) {
+				name = name1;
+				id = id1;
+			}
+
+			public void showInfo() {
+				System.out.println(name);
+				System.out.println(id);
+				System.out.println(couList.size());
+				for (int i = 0; i < couList.size(); i++) {
+					System.out.println(couList.get(i).getName());
+					System.out.println(couList.get(i).getID());
+				}
+			}
+
+			public void addCourse(String name1, String id1) {
+				couList.add(new Course(name1, id1));
+			}
+
+		}
+		class Course {
+
+			private String couName, couID, teacher;
+			private int score;
+
+			public Course(String name1, String id1) {
+				couName = name1;
+				couID = id1;
+			}
+
+			public String getName() {
+				return couName;
+			}
+
+			public String getID() {
+				return couID;
+			}
+		}
